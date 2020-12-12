e139e4ddfee35af7baf7764d81b7a37a6d8d8a1c
ROBERT-COSMIN IONAȘC <robert.ionasc@stud.ubbcluj.ro>
2020-12-12 13:01:15 +0200

    Add project files.

---
+++ PersonalAgenda/PersonalAgenda/NotePagexaml.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using PersonalAgenda.Models;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalAgenda
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotePagexaml : ContentPage
+    {
+        public NotePagexaml()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Agenda)BindingContext;
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Agenda)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+    }
+}
